3a22c668559c59b091353c066f49401b2bf981d4
AndresCarvajallx <andresfelipecarvajalduran3@gmail.com>
2023-10-05 01:23:12 -0500

    When icon is clicked so change the color

--- app/src/main/java/com/uflow/uflow/ui/navigation/components/BottomNavigationBar.kt
+++ app/src/main/java/com/uflow/uflow/ui/navigation/components/BottomNavigationBar.kt
@@ -1,13 +1,12 @@
 package com.uflow.uflow.ui.navigation.components
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -39,14 +38,15 @@ fun BottomNavigationBar(
                     )
                 },
                 icon = {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        screen.icon?.let {
-                            Icon(
-                                it,
-                                contentDescription = screen.title
-                            )
+                    Icon(
+                        imageVector = screen.icon,
+                        contentDescription = null,
+                        tint = if (screen.route == currentDestination?.route) {
+                            MaterialTheme.colorScheme.onPrimaryContainer
+                        } else {
+                            MaterialTheme.colorScheme.onSurfaceVariant
                         }
-                    }
+                    )
                 },
                 selected = currentDestination?.route == screen.route,
                 alwaysShowLabel = true,

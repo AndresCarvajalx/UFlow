666c61206b9f7d2c3d4b4e628a4964c7b7c6e60d
AndresCarvajallx <andresfelipecarvajalduran3@gmail.com>
2023-10-05 00:56:55 -0500

    Change in parameter that can be null, ImageVector? to ImageVector

--- app/src/main/java/com/uflow/uflow/ui/navigation/Screen.kt
+++ app/src/main/java/com/uflow/uflow/ui/navigation/Screen.kt
@@ -7,7 +7,7 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Timer
 import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class Screen(val route: String, val title:String = "", val icon: ImageVector? = null){
+sealed class Screen(val route: String, val title:String = "", val icon: ImageVector){
     object TasksScreen: Screen("task_screen", "Task", icon= Icons.Filled.Checklist)
     object TimerScreen: Screen("timer_screen", "Timer", icon=Icons.Filled.Timer)
     object HomeworkScreen: Screen("home_work_screen", "Work", icon=Icons.Filled.CalendarMonth)
